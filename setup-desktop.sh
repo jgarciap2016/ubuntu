8e5c0146541985b3fca932e7becdaa3f941c7fe2
jgarciap2016 <msonjgarciap2014@gmail.com>
2024-02-28 23:50:39 -0600

    setup-desktop.sh
    La instalación del escritorio de Ubuntu en modo gráfico pero muy sencillo, generalmente sirve para el uso de Servers.

---
+++ setup-desktop.sh
@@ -0,0 +1,54 @@
+#
+#   BIT @ BIT COMPUTADORAS
+#   
+#   JAVIER GARCÍA PÉREZ
+#
+#   SCRIPT DE CONFIGURACIÓN DE LINUX UBUNTU
+#
+#
+
+# Me cambio al superusuario
+sudo su
+
+
+# Se actualizan los archivos del sistema
+sudo apt-get update && sudo apt-get upgrade
+
+# Se instalan los programas del escritorio
+sudo apt-get install xorg
+sudo apt-get install openbox 
+sudo apt-get install obconf 
+sudo apt-get install obmenu 
+sudo apt-get install tint2 
+sudo apt-get install nitrogen
+sudo apt-get install conky
+sudo apt-get install xcompmgr
+
+# Se instalan aplicaciones del sistema
+sudo apt-get install pcmanfm
+sudo apt-get install cairo-dock
+sudo apt-get install cairo-dock-plug-ins
+sudo apt-get install kate
+
+sudo apt-get install firefox
+
+# Se instala OpenGl
+sudo apt-get install freeglut3
+sudo apt-get install freeglut3-dev
+sudo apt-get install binutils-gold
+
+# Se configura el openbox
+cd .config
+mkdir openbox
+cd openbox
+
+curl -O https://raw.githubusercontent.com/jgarciap2016/ubuntu/main/autostart.sh
+
+cd ..
+cd ..
+
+sudo apt-get install xinit
+
+
+#Termina la ejecución de los comandos de la configuración
+exit
