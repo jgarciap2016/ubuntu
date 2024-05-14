362f7331dfc4343eb75e040bf53cf5114094bad4
jgarciap2016 <msonjgarciap2014@gmail.com>
2024-05-13 20:33:41 -0600

    Aplicaciones
    Programas de ubuntu.

---
+++ setup-netbeans.sh
@@ -0,0 +1,31 @@
+
+#   JAVIER GARCÍA PÉREZ
+#
+#   BIT @ BIT COMPUTADORAS
+#   
+#
+#   SCRIPT DE CONFIGURACIÓN DE LINUX UBUNTU
+#
+#
+
+# Instalación de Netbeand y java 8
+
+
+# Java 8
+
+sudo add-apt-repository ppa:webupd8team/java
+
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+
+# Netbeand
+
+
+sudo add-apt-repository ppa:vajdics/netbeans-installer
+
+sudo apt update
+
+sudo apt install netbeans-installer
+
+
